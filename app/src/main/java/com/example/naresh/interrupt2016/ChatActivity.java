2f7b6a28ee22dff95ac786a7db6fa7223962caaa
adithya2509m <madithya2509@gmail.com>
2016-09-01 00:00:13 +0530

    chatbot and transport

--- app/src/main/java/com/example/naresh/interrupt2016/ChatActivity.java
+++ app/src/main/java/com/example/naresh/interrupt2016/ChatActivity.java
@@ -2,7 +2,7 @@ package com.example.naresh.interrupt2016;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
