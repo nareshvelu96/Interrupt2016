5b717d374712618b4bf4734cfd56ff91a2cf13b0
mathsank <mathsank@gmail.com>
2016-08-31 18:22:37 +0530

    aboutInterrupt final

--- app/src/main/java/com/example/naresh/interrupt2016/aboutInterrupt.java
+++ app/src/main/java/com/example/naresh/interrupt2016/aboutInterrupt.java
@@ -8,6 +8,10 @@ import android.os.Build;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -52,26 +56,33 @@ public class aboutInterrupt extends Fragment  {
         Typeface fontRobo4 = Typeface.createFromAsset(getActivity().getAssets(),"fonts/DroidSans.ttf");
         textView4.setTypeface(fontRobo4);
 
-
-
-
-
-
-
-
         if (mArguments != null){
             ImageView img = (ImageView) view.findViewById(R.id.image_header);
             img.setImageResource(mArguments.getInt(ARG_IMAGE_RES));
         }
-        return view;
-    }
+        Button event= (Button)view.findViewById(R.id.onclick_event);
+        event.setClickable(true);
+        event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.app.FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, new events_fragment()).commit();
+
+
+                //setTitle(navMenuTitles[position]);
+
+            }
+
+        });
+            return view;
+        }
 
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
 
-        AppCompatActivity a = (AppCompatActivity) context;
+        Activity a = (Activity) context;
 
         mArguments = getArguments();
         Drawable actionBarBg = getActivity().getResources().getDrawable(R.drawable.headerbg);
@@ -84,4 +95,5 @@ public class aboutInterrupt extends Fragment  {
         mFadingHelper.initActionBar(a);
 
     }
+
 }
