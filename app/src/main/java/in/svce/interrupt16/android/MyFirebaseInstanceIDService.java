78a30e007f9fc2194c0bb23daceaf2557708cf69
mathsank <mathsank@gmail.com>
2016-09-19 18:17:38 +0530

    Firebase Cloud Messaging single device

---
+++ app/src/main/java/in/svce/interrupt16/android/MyFirebaseInstanceIDService.java
@@ -0,0 +1,29 @@
+package in.svce.interrupt16.android;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+/**
+ * Created by Mathangi on 19-09-2016.
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+
+}
